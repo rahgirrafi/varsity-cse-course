69cdb5d1ce253e4478c57a2ad34176229a330abb
rafi <rahgirrafi>
2023-08-12 15:32:55 +0600

    done 5

--- 5.c
+++ 5.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
-#include<stdbool.h>
-int is_present(int *arr[],int elementNnm, int check);
-int main(){
-    int arr[50];
+#include <stdbool.h>
+int main() {
+    int n, duplicate;
+    scanf("%d", &n);
 
-    
+    int marker[n];
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        marker[i]= -1;
+    }
+    bool is_duplicate = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        is_duplicate = false;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[i]==arr[j])
+            {
+                marker[j]= 0;
+                is_duplicate = true;
+            }
+            
+        }
+        if(marker[i]!= 0 && is_duplicate==true)
+        {
+            marker[i] = 1;
+        }
+    }
+
+    int count =0;
+     for (int i = 0; i < n; i++)
+     {
+        if(marker[i] ==1)
+            count++;
+     }
+     
+     printf("%d",count);
+
+    return 0;
 }
+
+
